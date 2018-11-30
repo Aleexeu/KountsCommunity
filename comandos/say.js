b5f6c5df1df7d66a7bcc817325e29d5aac5000cb
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-30 21:31:42 -0200

    Update say.js

--- comandos/say.js
+++ comandos/say.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = (bot, message, args) => {
     if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Infelizmente você não tem permissão!");
-    message.delete();
+    message.delete()
     message.channel.send(args.join(' '))
 }
 
