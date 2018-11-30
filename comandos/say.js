d96ee3a09e8d8e0ff6775e92487584723a80ebf6
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-30 19:21:12 -0200

    Add files via upload

--- comandos/say.js
+++ comandos/say.js
@@ -1,9 +1,17 @@
 const Discord = require("discord.js");
 
-exports.run = (bot,message,args) => {
-        if(!message.guild.member(message.author.id).hasPermissions("BAN_MEMBERS")) return message.reply("Você não tem permissão de usar esse comando")
-    message.delete();
-    message.channel.send(args.join(" "))
+exports.run = (bot, message, args) => {
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Infelizmente você não tem permissão!");
+    message.delete()
+    message.channel.send('@everyone | @here')
+    let embed = new Discord.RichEmbed()
+    .setTitle('**AVISO**')
+    .setDescription(args.join(' '))
+    .setThumbnail(bot.user.displayAvatarURL)
+    .setColor('#00374')
+    .setFooter(`Aviso enviado por: ${message.author.tag}`, bot.user.displayAvatarURL);
+
+    message.channel.send(embed)
 }
 
 exports.help = {
